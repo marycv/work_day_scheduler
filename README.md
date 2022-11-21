753ad19c98d218c7890d6fc5864b3ab67849aa33
MC Villoso <mcvilloso@gmail.com>
2022-11-20 19:11:28 -0800

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # work_day_scheduler
+##  Table of Contents
+[Author Name](#author-name) <br>
+[GitHub Repo](#github-repo) <br>
+[Project Description](#project-description) <br>
+[Preview](#preview) <br>
+[Contact](#contact)
+## Author Name
+Mary Villoso
+## GitHub Repo
+[marycv](https://github.com/marycv/work_day_scheduler)
+## Project Description
+This project is simple calendar application that allows a user to save events for each hour of the day.
+## Preview
+https://marycv.github.io/work_day_scheduler
+## Contact
+Questions? Contact me at mcvilloso@gmail.com 
+
--- script.js
+++ script.js
@@ -14,25 +14,18 @@ var containerEl = $('#timeEntries');
     // Compare i to current hour to determine if this hour is in the past, present, or future
 function currentTense() {
     // DECLARE current hour
-    var currentHour = moment().format('H');
+    var currentHour = moment().hours();
     
-    // Time Block array
-    
-    // var timeBlock = document.querySelectorAll('.time-block');
-    
-    // var timeBlockArray = timeBlock[i];
-    // console.log(timeBlockArray);
-    // Get textArea element to add tense
+    // Create an array of the time-block divs
     var timeBlockArray = document.querySelectorAll('.time-block');
-    console.log(timeBlockArray);
     
-    for ( var i=0; i < timeBlockArray.length; i++)
-    
-        var textArea = document.getElementById("textArea");
-        // for (var i = 8; i < 18; i++) {
+    for ( var i=0; i < timeBlockArray.length; i++) {
+        // Declare variable to add class 'past', 'preset', or 'future' to the textArea
+        var textArea = timeBlockArray[i].children[1];
+        // Declare variable to pull the timeblock hour
+        var timeBlock = timeBlockArray[i].id;
+        
         if (timeBlock < currentHour) {
-            // change color of textarea to grey;
-            // timeBlock.addClass('past');
             textArea.classList.add('past');
         } else if (timeBlock === currentHour) {
             // change color of text area to red;
@@ -40,37 +33,37 @@ function currentTense() {
             // timeBlock.addClass('present');
         } else {
             // change color of text area to green;
+            textArea.classList.remove('past');
+            textArea.classList.remove('present');
             textArea.classList.add('future');
-            // timeBlock.removeClass('past');
-            // timeBlock.removeClass('present');
-            // timeBlock.addClass('future');
         }
-};
-// };
+    };
+}
 
 currentTense();
 
 
 // Set up a "click" event listener on the container
-containerEl.on("click", "[data-hour]", function(event) {
+// button.on('click', function() {
 
-    // Fetch the hour from the clicked button's (event.target) "data-hour" attribute
+// })
+// containerEl.on("click", "[data-hour]", function(event) {
+
+//     // Fetch the hour from the clicked button's (event.target) "data-hour" attribute
 
 
-    // Use the hour to create the key for local storage
+//     // Use the hour to create the key for local storage
 
 
-    // From the clicked button, traverse the DOM to the nearby <textarea> to fetch its value
+//     // From the clicked button, traverse the DOM to the nearby <textarea> to fetch its value
     
-    // Use the key and the value to save into localStorage
+//     // Use the key and the value to save into localStorage
 
-    //localStorage.setItem('hour', key);
-    //localStorage.setItem('value', value);
-
-
+//     //localStorage.setItem('hour', key);
+//     //localStorage.setItem('value', value);
 
 
 
-});
 
-    // Save an hour to local storage, this will be attached to click event listener
+
+// });
